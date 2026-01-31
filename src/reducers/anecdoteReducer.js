a42900eeb88377367589a07ceb81123cbc146380
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-31 19:33:55 -0300

    redux-anecdotes

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -47,4 +47,11 @@ export const voteAnecdote = id => {
   }
 }
 
+export const createAnecdote = content => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: { content }
+  }
+}
+
 export default reducer
