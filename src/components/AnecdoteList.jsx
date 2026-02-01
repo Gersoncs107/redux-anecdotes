e281658b81e51594919745128d5761d571443d6b
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-31 22:00:21 -0300

    redux-anecdotes

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -9,15 +9,18 @@ const AnecdoteList = () => {
 
   return (
     <div>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+        {anecdotes
+        .slice()                            
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
