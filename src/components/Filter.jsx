a3854bec5f450a4e7dd9971dd021a2692c3e4313
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-04 22:32:17 -0300

    redux-anecdotes

---
+++ src/components/Filter.jsx
@@ -0,0 +1,23 @@
+const Filter = () => {
+  const handleChange = (event) => {
+    // input-field value is in variable event.target.value
+    const value = event.target.value
+    // We dispatch an action to set the filter in the state
+    // The action is created by calling the action creator setFilter
+    // which is imported from the filterReducer
+    // We use the useDispatch hook to get the dispatch function
+    // and then we call it with the action created by setFilter
+    dispatch(setFilter(value))
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
