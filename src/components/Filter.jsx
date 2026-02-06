40b8814af837dccfd8f15c17d692a50779509fff
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-05 22:19:35 -0300

    redux-anecdotes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,22 +1,15 @@
 import { useDispatch } from 'react-redux'
-import { setFilter } from '../reducers/anecdoteReducer'
+import { filterChange } from '../reducers/filterReducer'
 
 const Filter = () => {
   const dispatch = useDispatch()
+  
   const handleChange = (event) => {
-    // input-field value is in variable event.target.value
     const value = event.target.value
-    // We dispatch an action to set the filter in the state
-    // The action is created by calling the action creator setFilter
-    // which is imported from the filterReducer
-    // We use the useDispatch hook to get the dispatch function
-    // and then we call it with the action created by setFilter
-    dispatch(setFilter(value))
+    dispatch(filterChange(value))
   }
 
-  const style = {
-    marginBottom: 10
-  }
+  const style = { marginBottom: 10 }
 
   return (
     <div style={style}>
