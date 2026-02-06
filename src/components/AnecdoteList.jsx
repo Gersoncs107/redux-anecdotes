33b8c14b2cb6c5910d2d5324e7f4289c6a2633fb
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-05 21:56:07 -0300

    redux-anecdotes

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -2,7 +2,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === 'ALL' || filter === '') {
+      return anecdotes
+    }
+    return anecdotes.filter(a => 
+      a.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  })
+  
   const dispatch = useDispatch()
   
   const vote = id => dispatch(voteAnecdote(id))
