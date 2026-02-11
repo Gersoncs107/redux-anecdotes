3f80768e0d8cce4e6bcd47c8980029e4acbe7ec8
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-10 22:25:08 -0300

    redux-anecdotes

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -13,21 +13,3 @@ const filterSlice = createSlice({
 export const { filterChange } = filterSlice.actions
 
 export default filterSlice.reducer
-
-// const filterReducer = (state = 'ALL', action) => {
-//   switch (action.type) {
-//     case 'SET_FILTER':
-//       return action.filter
-//     default:
-//       return state
-//   }
-// }
-
-// export const filterChange = filter => {
-//   return {
-//     type: 'SET_FILTER',
-//     filter,
-//   }
-// }
-
-// export default filterReducer
