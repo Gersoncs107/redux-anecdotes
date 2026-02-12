2959efe385b8be4a189db6507518fb37d3ca475c
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-12 19:44:26 -0300

    redux-anecdotes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
+import  Notification from "./Notification";
 
 const Filter = () => {
   const dispatch = useDispatch()
