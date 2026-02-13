8fe976dfd3b35805d7076accd98d6a42ee00b4ad
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-12 21:55:30 -0300

    redux-anecdotes

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from 'react-redux'
+
 const Notification = () => {
+  const notification = useSelector(state => state.notification)
+
   const style = {
     border: 'solid',
     padding: 10,
@@ -6,9 +10,11 @@ const Notification = () => {
     marginBottom: 10
   }
 
+  if (!notification) return null
+
   return (
     <div style={style}>
-      render here notification...
+      {notification}
     </div>
   )
 }
