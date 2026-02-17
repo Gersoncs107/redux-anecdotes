38650c21c6c805d330192644b0f1a5f1c4e63ba9
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-16 22:18:26 -0300

    redux-anecdotes

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from "../services/anecdotes"
 
 
 const anecdoteSlice = createSlice({
@@ -24,6 +25,14 @@ const anecdoteSlice = createSlice({
     }
   }
 })
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 
 export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
 
